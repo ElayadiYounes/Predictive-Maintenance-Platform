fdab2549c84b7fb33608daf4ebb1a18c0ee91202
ELAYADI Younes <younesselayadi05@gmail.com>
2026-07-28 20:34:03 +0200

    amélioration minio_client et mysql_client + creation extract_inspection en utilisent model common

---
+++ jobs/bronze/db/extract_inspection.py
@@ -0,0 +1,47 @@
+from jobs.common.logger import logger
+from jobs.common.config import settings
+from jobs.common.mysql_client import MySQLClient
+from jobs.common.minio_client import MinioStorageClient
+
+
+def extract_inspection() -> None:
+    """
+    Pipeline Bronze :
+        MySQL  --->  Pandas DataFrame  --->  MinIO Bronze
+    """
+    logger.info("="*70)
+    logger.info("BRONZE INGESTION START")
+    logger.info("="*70)
+
+    mysql_client = MySQLClient()
+    minio_client = MinioStorageClient()
+    try:
+        # Vérification des infrastructures
+        mysql_client.check_connection()
+        minio_client.check_connection()
+
+        logger.info("Lecture de la table inspection ...")
+
+        df = mysql_client.read_table("inspection")
+
+        logger.info(f"Extraction terminée ({len(df):,} lignes).")
+
+        minio_client.upload_dataframe(
+            dataframe=df,
+            bucket_name=settings.BRONZE_BUCKET,
+            file_name="vibration.parquet",
+            prefix="inspection",
+        )
+
+        logger.info("=" * 70)
+        logger.success("Ingestion Bronze terminée.")
+        logger.info("=" * 70)
+
+    except Exception:
+        logger.exception("Le pipeline Bronze a échoué.")
+        raise
+
+
+
+if __name__ == "__main__":
+    extract_inspection()
--- jobs/common/minio_client.py
+++ jobs/common/minio_client.py
@@ -1,10 +1,15 @@
 import boto3
 from botocore.client import Config
 from botocore.exceptions import ClientError
+import pandas as pd
+from io import BytesIO
+from datetime import datetime, UTC
+
 
 from jobs.common.config import settings
 from jobs.common.logger import logger
-from jobs.common.exceptions import DataLakeConnectionError
+from jobs.common.exceptions import DataLakeConnectionError, DataLakeBucketNotFoundError
+
 
 
 class MinioStorageClient:
@@ -35,6 +40,7 @@ class MinioStorageClient:
             logger.exception("Impossible d'initialiser le client MinIO.")
             raise
 
+#################### Vérifie la connexion #####################
     def check_connection(self) -> None:
         """Vérifie si le Data Lake est en ligne et accessible."""
         try:
@@ -46,9 +52,93 @@ class MinioStorageClient:
                 str(e)
             )
 
-#0000000000000000000000000000000000000000000000000000000000000000000000000000000000
-    #on doit definit les methodes d'upload ici pour centraliser le processus d'ingestion
-    #par exemple
-    """upload_json(data, bucket, object_name) : Reçoit un dictionnaire Python (vos données universitaires brutes fraîchement extraites d'une API),
-     le convertit automatiquement en texte JSON et l'envoie directement dans la couche Bronze (raw/) de MinIO,
-      sans avoir à sauvegarder de fichier temporaire sur votre disque Windows."""
+################## charger dataFrame #########################
+    def upload_dataframe(self, dataframe: pd.DataFrame, bucket_name: str, file_name: str, prefix : str) -> None:
+        """
+        Convertit un DataFrame Pandas en Parquet puis
+        l'envoie directement dans MinIO.
+
+        Parameters
+        ----------
+        dataframe : pd.DataFrame
+            DataFrame à sauvegarder.
+
+        bucket_name : str
+            Bucket cible.
+
+        file_name : str
+            le nom de fichier
+            Exemple :
+            maintenance.parquet
+        prefix : str
+            generalement le nom de table cible
+        """
+        now = datetime.now(UTC)
+        year = now.strftime("%Y")
+        month = now.strftime("%m")
+        day = now.strftime("%d")
+
+        logger.info(f"Upload du DataFrame {file_name} vers {bucket_name}")
+
+        try:
+            # Vérification connexion
+            self.check_connection()
+
+            # Vérification bucket
+            self.s3_client.head_bucket(Bucket=bucket_name)
+
+            object_path = (
+                f"{prefix}/"
+                f"{year}/{month}/{day}/"
+                f"{file_name}"
+            )
+
+            if dataframe.empty:
+                logger.warning("Le DataFrame est vide.")
+                return
+
+            # Conversion mémoire et stockage vers minIo
+            with BytesIO() as buffer:
+
+                dataframe.to_parquet(
+                    buffer,
+                    engine="pyarrow",
+                    index=False,
+                )
+
+                buffer.seek(0)
+
+                self.s3_client.upload_fileobj(
+                    Fileobj=buffer,
+                    Bucket=bucket_name,
+                    Key=object_path,
+                )
+
+            logger.success(f"DataFrame envoyé avec succès : {bucket_name}/{object_path}")
+
+        except ClientError as e:
+
+            error_code = (
+                e.response
+                .get("Error", {})
+                .get("Code")
+            )
+
+            if error_code in ("404", "NoSuchBucket"):
+                logger.exception(f"Bucket introuvable : {bucket_name}")
+
+                raise DataLakeBucketNotFoundError(
+                    f"Bucket '{bucket_name}' introuvable.",
+                    str(e),
+                )
+
+            logger.exception("Erreur MinIO.")
+
+            raise DataLakeConnectionError(
+                "Erreur lors de l'upload vers MinIO.",
+                str(e),
+            )
+
+        except Exception:
+            logger.exception("Erreur inattendue lors de l'upload.")
+            raise
