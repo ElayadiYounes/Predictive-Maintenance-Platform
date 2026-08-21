d43f9f7c2808e66700d355cd2ca0ca753a1738dd
ELAYADI Younes <younesselayadi05@gmail.com>
2026-08-21 17:26:47 +0200

    j'ai fixé le problème de concervation de données par Metabase pour collaborer parfaitement sur la phase de visualisation

--- docker/postgres/init/01-init-databases.sql
+++ docker/postgres/init/01-init-databases.sql
@@ -7,3 +7,8 @@ SELECT 'CREATE DATABASE airflow'
 WHERE NOT EXISTS (
     SELECT FROM pg_database WHERE datname = 'airflow'
 )\gexec
+
+SELECT 'CREATE DATABASE metabase_db'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'metabase_db'
+)\gexec
