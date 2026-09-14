97cbfcb67edb75e3aaad9ab23cd697a690024d60
ELAYADI Younes <younesselayadi05@gmail.com>
2026-09-14 11:15:00 +0200

    j'ai ajouté script d'execution de l'etape silver_to_gold

---
+++ airflow/scripts/run_silver_to_gold.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Script d'automatisation Airflow - Étape 3 : Calcul des Tables Gold
+# ------------------------------------------------------------------
+set -e
+
+echo "⏳ [Airflow] Lancement de la transformation Silver → Gold..."
+
+# Exécution de votre script de modélisation Gold (ajustez le nom du script si nécessaire)
+docker compose --env-file .env.dev -f docker-compose.dev.yml exec -T spark-master spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  --conf spark.driver.host=spark-master \
+  /app/jobs/gold/build_gold.py
+
+echo "✅ [Airflow] Tables Gold générées et disponibles."
