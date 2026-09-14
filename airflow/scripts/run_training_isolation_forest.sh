53ea644e8ced995ca756ba0e34e74ae28c559c3a
ELAYADI Younes <younesselayadi05@gmail.com>
2026-09-14 11:29:06 +0200

    j'ai ajouté script pour re-entrainement isolation forest

---
+++ airflow/scripts/run_training_isolation_forest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Script d'automatisation Airflow - Étape 3 : Isolation forest ML
+# ------------------------------------------------------------------
+
+
+set -e
+
+echo "[Airflow] Démarrage du ré-entraînement  de l'Isolation Forest..."
+
+docker compose \
+    --env-file .env.dev \
+    -f docker-compose.dev.yml \
+    exec -T ml \
+    python /app/jobs/ml/run_isolation_forest.py
+
+echo "[Airflow] Entraînement Isolation Forest terminé avec succès."
+
