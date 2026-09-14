70fb0dacbbf66643c953376806db68a9b732a8e9
ELAYADI Younes <younesselayadi05@gmail.com>
2026-09-14 11:11:21 +0200

    j'ai ajouté le script corespond à bronze_to_silver

---
+++ airflow/scripts/run_bronze_to_silver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Script d'automatisation Airflow - Étape 2 : Transformation Silver
+# ------------------------------------------------------------------
+set -e
+
+echo "⏳ [Airflow] Lancement de la transformation Bronze → Silver..."
+
+docker compose --env-file .env.dev -f docker-compose.dev.yml exec -T spark-master spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  --conf spark.driver.host=spark-master \
+  /app/jobs/silver/transform_inspection.py
+
+echo "✅ [Airflow] Couche Silver mise à jour avec succès."
