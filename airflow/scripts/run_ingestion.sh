1591cd93e206f4fc82258784a235214d0b31de59
ELAYADI Younes <younesselayadi05@gmail.com>
2026-09-14 11:02:18 +0200

    j'ai construit l'arborecsence de la couche final DAG + j'ai implémenté premiere script .sh d'execution premiere étape ingestion

---
+++ airflow/scripts/run_ingestion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Script d'automatisation Airflow - Étape 1 : Ingestion Bronze
+# ------------------------------------------------------------------
+
+# Force l'arrêt immédiat du script au moindre problème technique
+set -e
+
+echo "⏳ [Airflow] Déclenchement de l'extraction des inspections..."
+
+# Exécution non-interactive directe du script Python dans le conteneur
+docker compose \
+    --env-file .env.dev \
+    -f docker-compose.dev.yml \
+    exec -T ingestion \
+    python /app/jobs/bronze/db/extract_inspection.py
+
+echo "✅ [Airflow] Ingestion Bronze terminée avec succès."
+
